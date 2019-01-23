88b7bc849651f44852b3a241ab49e9612d6108c5
Johnny <syonip@gmail.com>
2019-01-23 21:51:39 +0200

    added picture downloading

--- app/src/main/java/com/example/myapplication/PictureContent.java
+++ app/src/main/java/com/example/myapplication/PictureContent.java
@@ -1,12 +1,13 @@
 package com.example.myapplication;
 
+import android.app.DownloadManager;
+import android.content.Context;
 import android.net.Uri;
+import android.os.Environment;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Helper class for providing sample content for user interfaces created by
@@ -29,6 +30,39 @@ public class PictureContent {
                 }
             }
         }
+    }
+
+//    private void deleteSavedImages() {
+//        File dir = new File(this.getFilesDir()+ "/images/");
+//        if (dir.exists()) {
+//            File[] files = dir.listFiles();
+//            for (File file : files) {
+//                String absolutePath = file.getAbsolutePath();
+//                String extension = absolutePath.substring(absolutePath.lastIndexOf("."));
+//                if (extension.equals(".jpg")) {
+//                    file.delete();
+//                }
+//            }
+//        }
+//        ITEMS.clear();
+//        recyclerView.getAdapter().notifyDataSetChanged();
+//    }
+
+    public static void downloadRandomImage(DownloadManager downloadmanager, Context context) {
+
+        long ts = System.currentTimeMillis();
+        Uri uri = Uri.parse("https://picsum.photos/1000/500/?random");
+
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        request.setTitle("My File");
+        request.setDescription("Downloading");
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
+        request.setVisibleInDownloadsUi(false);
+        String fileName = ts + ".jpg";
+        request.setDestinationInExternalFilesDir(context, Environment.DIRECTORY_DOWNLOADS, fileName);
+
+
+        downloadmanager.enqueue(request);
     }
 
     private static void loadImage(File file) {
--- app/src/main/java/com/example/myapplication/ScrollingActivity.java
+++ app/src/main/java/com/example/myapplication/ScrollingActivity.java
@@ -1,9 +1,18 @@
 package com.example.myapplication;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
@@ -14,11 +23,49 @@ import android.view.MenuItem;
 
 import java.io.File;
 
+import static com.example.myapplication.PictureContent.downloadRandomImage;
 import static com.example.myapplication.PictureContent.loadSavedImages;
 
 public class ScrollingActivity extends AppCompatActivity
         implements ItemFragment.OnListFragmentInteractionListener{
+    private static final int REQUEST_CODE_READ = 2;
+    private static final int REQUEST_CODE_WRITE = 3;
     private RecyclerView recyclerView;
+    private ScrollingActivity context;
+    private boolean writePermissionsGranted = false;
+    private boolean readPermissionsGranted = false;
+
+    public  boolean checkReadStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                readPermissionsGranted = true;
+                return true;
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE_READ);
+                return false;
+            }
+        }
+        else {
+            return true;
+        }
+    }
+
+    public  boolean checkWriteStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                writePermissionsGranted = true;
+                return true;
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_WRITE);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            return true;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +74,23 @@ public class ScrollingActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        checkReadStoragePermissionGranted();
+        checkWriteStoragePermissionGranted();
+
+        context = this;
+        final DownloadManager downloadmanager = (DownloadManager) getSystemService(context.DOWNLOAD_SERVICE);
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        downloadRandomImage(downloadmanager, context);
+                    }
+                });
             }
         });
 
@@ -41,6 +99,32 @@ public class ScrollingActivity extends AppCompatActivity
             recyclerView = (RecyclerView) currentFragment.getView();
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CODE_READ:
+                if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                    readPermissionsGranted = true;
+                } else {
+                }
+                break;
+
+            case REQUEST_CODE_WRITE:
+                if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                    writePermissionsGranted = true;
+
+                } else {
+
+                }
+                break;
+        }
+        if (readPermissionsGranted && writePermissionsGranted) {
+            init();
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -66,12 +150,25 @@ public class ScrollingActivity extends AppCompatActivity
     @Override
     protected void onResume() {
         super.onResume();
-        final Activity context = this;
+
+        if (readPermissionsGranted && writePermissionsGranted) {
+            init();
+        }
+    }
+
+    private void init() {
+        BroadcastReceiver onComplete=new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                recyclerView.getAdapter().notifyDataSetChanged();
+            }
+        };
+
+        context.registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
 
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                loadSavedImages(new File(context.getFilesDir() + "/images/"));
+                loadSavedImages(context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS));
                 recyclerView.getAdapter().notifyDataSetChanged();
             }
         });
